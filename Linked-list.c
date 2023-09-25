76d305dced82bcde1beaf63bdf98866121b8a29f
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2023-09-25 18:19:47 +0330

    adding linked list

--- Linked-list.c
+++ Linked-list.c
@@ -2,38 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
-    struct Node *next;
+    struct Node* next;
 };
 
-// Function to create a linked list of given array
-struct Node* createNode(int data, struct Node *next) {
-    struct Node* node = (struct Node*) malloc(sizeof(struct Node));
-    node->data = data;
-    node->next = next;
-    return node;
+
+// Function to create a node
+void push(struct Node** headRef, int data)
+{
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+ 
+    temp->data = data;
+    temp->next = *headRef;
+ 
+    *headRef = temp;
 }
+
+
+// Function to traverse the array and pass every value of the array to push()
+struct Node* createList(int values[], int n)
+{
+	struct Node* head = NULL;
+	 	
+    for (int i = n - 1; i >= 0; i--) {
+        push(&head, values[i]);
+    }
+ 
+    return head;
+}
+
 
 // Function to print elements of linked list
-void printLinkedlist(struct Node *node) {
+void printList(struct Node *node) {
     printf("Linked list: ");
     while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
+    printf("%d -> ", node->data);
+    // printf("%p ", node->next);
+    node = node->next;
     }
+    printf("NULL");
 }
 
-int main(){
-    int data[] = {10, 20, 30};
-    int n = sizeof(data)/sizeof(data[0]);
-    struct Node* head = NULL;
-    struct Node* ptr = NULL;
-    
-    for(int i = n-1; i>=0; i--){
-        ptr = createNode(data[i], ptr);
-        head = ptr;
-    }
 
-    printLinkedlist(head);
+int main(void)
+{
+    int values[] = {10, 20, 30};
+    int n = sizeof(values)/sizeof(values[0]);
+ 
+    struct Node* head = createList(values, n);
+ 
+    printList(head);
 }
